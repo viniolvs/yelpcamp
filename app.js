b765006e888882a32bad7ae1b071eab43802aca1
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-11-22 16:39:36 -0300

    (add) delete campground

--- app.js
+++ app.js
@@ -28,11 +28,13 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+//show all campgrounds
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 })
 
+//add campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
@@ -43,11 +45,13 @@ app.post('/campgrounds', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 })
 
+//show campground
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 })
 
+//edit and update campground
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
@@ -57,6 +61,13 @@ app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await  Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
+})
+
+//delete campground
+app.delete('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 app.listen(3000, () => {
     console.log("LISTEN TO PORT 3000");
